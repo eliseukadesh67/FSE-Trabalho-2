9d435ef4162009a292e2956505c3a28fa589a141
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2023-01-20 17:30:08 -0300

    adicionando caculo de crc.

---
+++ inc/crc16.h
@@ -0,0 +1,18 @@
+/*
+ * crc16.h
+ *
+ *  Created on: 18/03/2014
+ *      Author: Renato Coral Sampaio
+ */
+
+#ifndef CRC16_H_
+#define CRC16_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+short CRC16(short crc, char data);
+short calcula_CRC(unsigned char *commands, int size);
+int validate_CRC(u_char *recieved_data);
+
+#endif /* CRC16_H_ */
